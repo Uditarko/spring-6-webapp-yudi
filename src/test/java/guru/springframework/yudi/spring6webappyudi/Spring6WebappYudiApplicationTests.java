1d55ebf0e719abf4150ac7ebb9284644b726ea27
Uditarko Dutta <uditarko.dutta@gmail.com>
2024-06-21 23:22:12 +0530

    Initial Commit

---
+++ src/test/java/guru/springframework/yudi/spring6webappyudi/Spring6WebappYudiApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.yudi.spring6webappyudi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6WebappYudiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
