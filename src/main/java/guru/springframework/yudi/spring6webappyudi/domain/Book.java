eff71625fce206aa58942f4b9a01e134f4ccf9bd
Uditarko Dutta <uditarko.dutta@gmail.com>
2024-06-22 07:57:50 +0530

    Created the base entity classes

---
+++ src/main/java/guru/springframework/yudi/spring6webappyudi/domain/Book.java
@@ -0,0 +1,40 @@
+package guru.springframework.yudi.spring6webappyudi.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id;
+    private String title;
+    private String isbn;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+}
