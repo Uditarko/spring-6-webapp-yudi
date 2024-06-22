4ba5fc10b466cbd43644b96e32969d8ae0ebe2b4
Uditarko Dutta <uditarko.dutta@gmail.com>
2024-06-22 08:26:52 +0530

    book and author many to many bidirectional relationship

--- src/main/java/guru/springframework/yudi/spring6webappyudi/domain/Book.java
+++ src/main/java/guru/springframework/yudi/spring6webappyudi/domain/Book.java
@@ -1,9 +1,8 @@
 package guru.springframework.yudi.spring6webappyudi.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.Set;
 
 @Entity
 public class Book {
@@ -12,6 +11,11 @@ public class Book {
     private Long Id;
     private String title;
     private String isbn;
+
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+    inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Set<Author> authors;
 
     public Long getId() {
         return Id;
